7c4a0a134f9119546a198a1c60121d77f0f7cc1a
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-10-04 23:32:31 +0900

    feat: 아폴로 클라이언트 구성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { ApolloProvider } from "@apollo/client";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ApolloWrapper } from "@/lib/apollo-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ApolloWrapper>{children}</ApolloWrapper>
+      </body>
     </html>
   );
 }
